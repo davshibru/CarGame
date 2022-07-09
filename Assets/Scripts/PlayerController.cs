524bdc495e9ed7340f3a77cbe03cd8b832d0c4ca
David Shibru <davshibru@gmail.com>
2022-07-09 21:33:33 +0600

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+
+        // Moving car
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
+        // Rotating car
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
+    }
+}
